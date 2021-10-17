37618c3b883b4f39a8ee71ccd9b687b98f7fc05b
Ferdinand Ismael <35696818+ferddecoded@users.noreply.github.com>
2021-10-17 18:02:03 -0400

    rm default state

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,5 +1,8 @@
 import { createContext } from 'react';
 
-const ThemeContext = createContext(['green', () => {}]);
+// not necessary to pass a default argument here
+// because of how its used in App.js, we set the value to useState (const theme = useState('darkblue'))
+// const ThemeContext = createContext(['green', () => {}]);
+const ThemeContext = createContext();
 
 export default ThemeContext;
